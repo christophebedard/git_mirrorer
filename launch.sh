21346087cfba8e04159708fd2ccdeee67a4bb4f7
Christophe Bedard <bedard.christophe@gmail.com>
2018-09-15 20:41:20 -0400

    Add helper launch script

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# params
+export ORIGIN_URL=https://github.com/USERNAME/REPO_ORIGIN.git
+export DESTINATION_URL=https://github.com/USERNAME/REPO_DESTINATION.git
+export BRANCHES_LIST=[branch1,branch2]
+
+docker run -d -it --rm \
+-e ORIGIN_URL \
+-e DESTINATION_URL \
+-e BRANCHES_LIST \
+-e UPDATE_PERIOD \
+--name git_mirrorer git_mirrorer:latest
